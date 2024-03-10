d3308b4087860551b97df36fe1f1cd3d7b253cb8
JunmingZhang <peterzhangjm@outlook.com>
2024-03-09 20:12:04 -0500

    Complete NFT for IP: 1. add max # supplies allow per member 2. add member and owner

--- contract/intellectualProperty.sol
+++ contract/intellectualProperty.sol
@@ -5,22 +5,84 @@ import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
 
 contract intellectualProperty is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
-    constructor(address initialOwner)
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    
+    uint256 public totalShares; // Total number of shares (members)
+    uint256 public maxSupplyPerMember;
+    mapping(address => uint256) public shares; // Mapping of member addresses to their share count
+    mapping(address => uint256) public numSupplied;
+
+    constructor(
+        address initialOwner,
+        address[] memory _members,
+        uint256 maxSupply
+    )
         ERC721("intellectualProperty", "IP")
         Ownable(initialOwner)
-    {}
-
-    function safeMint(address to, uint256 tokenId, string memory uri)
-        public
-        onlyOwner
     {
+        maxSupplyPerMember = maxSupply;
+        totalShares = _members.length; // Initialize totalShares with the number of members
+        for (uint256 i = 0; i < _members.length; i++) {
+            shares[_members[i]] = 1; // Each member initially has 1 share
+            numSupplied[_members[i]] = 0; // Each member has supplied 0 till contract construction
+        }
+    }
+
+    modifier onlyMember() {
+        require(shares[msg.sender] > 0, "Only members can call this function"); // Modifier to restrict access to members
+        _;
+    }
+
+
+    function safeMint(address to, string memory uri)
+        public
+        onlyMember
+    {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+
+        // ensure any member can only mint max number of NFTs allowed
+        require(numSupplied[msg.sender] < maxSupplyPerMember, "Sorry, all NFTs for this user have been minted!");
+        numSupplied[msg.sender]++;
+
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
+    function addMember(address newMember)
+        public 
+        onlyOwner
+    {
+        require(newMember != address(0) && shares[newMember] <= 0, "Invalid address"); // Ensure new member address is valid
+        shares[newMember]++; // Reduce old owner's share
+    }
+
+    function removeMember(address oldMember)
+        public
+        onlyOwner
+    {
+        // Ensure old member address is valid and is a valid member
+        require(oldMember != address(0) && shares[oldMember] > 0, "Invalid address");
+        shares[oldMember]--; // Decreases new owner's share
+        delete shares[oldMember];
+        delete numSupplied[oldMember];
+    }
+
     // The following functions are overrides required by Solidity.
+
+    function transferOwnership(address newOwner)
+        public
+        override(Ownable)
+        onlyOwner
+    {
+        require(newOwner != address(0), "Invalid address"); // Ensure new owner address is valid
+        transferOwnership(newOwner); // Transfer ownership using Ownable
+    }
 
     function tokenURI(uint256 tokenId)
         public
