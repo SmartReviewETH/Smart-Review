719a3255d8dd7d2c7c8ce564c6d31bc07032f7eb
Qi-001 <feihuvxv@gmail.com>
2024-03-12 15:14:32 -0400

    Redesign NFT as fractional NFT

--- contract/intellectualProperty.sol
+++ contract/intellectualProperty.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.s
 import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+import "contract/FractionalIP.sol";
+
 
 contract intellectualProperty is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
@@ -13,23 +15,33 @@ contract intellectualProperty is ERC721, ERC721URIStorage, ERC721Burnable, Ownab
     Counters.Counter private _tokenIdCounter;
     
     uint256 public totalShares; // Total number of shares (members)
-    uint256 public maxSupplyPerMember;
+    uint256 public maxSupply;
+    uint256 public numSupplied;
     mapping(address => uint256) public shares; // Mapping of member addresses to their share count
-    mapping(address => uint256) public numSupplied;
+    address[] public members;
+
+    struct _fnft{
+        uint256 tokenId;
+        address fractionalToken;
+    }
+
+    mapping(uint256 => _fnft) public nft;
 
     constructor(
         address initialOwner,
         address[] memory _members,
-        uint256 maxSupply
+        uint256[] memory supplies   // Member order and supply order should match
     )
         ERC721("intellectualProperty", "IP")
         Ownable(initialOwner)
     {
-        maxSupplyPerMember = maxSupply;
-        totalShares = _members.length; // Initialize totalShares with the number of members
+       // maxSupplyPerMember = maxSupply;
+        members = _members;
+        totalShares = 0; // Initialize totalShares with the number of members
         for (uint256 i = 0; i < _members.length; i++) {
-            shares[_members[i]] = 1; // Each member initially has 1 share
-            numSupplied[_members[i]] = 0; // Each member has supplied 0 till contract construction
+            shares[_members[i]] = supplies[i]; 
+            totalShares = totalShares + supplies[i];
+            //numSupplied[_members[i]] = 0; // Each member has supplied 0 till contract construction
         }
     }
 
@@ -38,6 +50,10 @@ contract intellectualProperty is ERC721, ERC721URIStorage, ERC721Burnable, Ownab
         _;
     }
 
+    modifier onlySingleMember() {
+        require(shares[msg.sender] == totalShares, "Only sole owner can call this function"); // Modifier to restrict access to sole owner
+        _;
+    }
 
     // add a new NFT
     function safeMint(address to, string memory uri)
@@ -47,32 +63,23 @@ contract intellectualProperty is ERC721, ERC721URIStorage, ERC721Burnable, Ownab
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
 
-        // ensure any member can only mint max number of NFTs allowed
-        require(numSupplied[msg.sender] < maxSupplyPerMember, "Sorry, all NFTs for this user have been minted!");
-        numSupplied[msg.sender]++;
+        // ensure only max number of NFTs allowed
+        require(numSupplied < maxSupply, "Sorry, all NFTs have been minted!");
+        numSupplied++;
 
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-    }
 
-    function addMember(address newMember)
-        public 
-        onlyOwner
-    {
-        require(newMember != address(0) && shares[newMember] <= 0, "Invalid address"); // Ensure new member address is valid
-        shares[newMember]++; // Increase old owner's share
-        numSupplied[newMember] = 0;
-    }
-
-    function removeMember(address oldMember)
-        public
-        onlyOwner
-    {
-        // Ensure old member address is valid and is a valid member
-        require(oldMember != address(0) && shares[oldMember] > 0, "Invalid address");
-        shares[oldMember]--; // Decreases new owner's share
-        delete shares[oldMember];
-        delete numSupplied[oldMember];
+        //Create a ERC20 Token Contract for this newly minted NFT
+        FractionalIP _fip = (new FractionalIP)();                                      //initialize
+        for (uint256 i = 0; i < members.length; i++) {
+            _fip.mint(members[i], shares[members[i]] * 1000000000000000000);   //now mint the fractional tokens and send it to the owner of this NFT
+        }
+                   
+        _fnft memory fnft;                                                          //constructor
+        fnft.tokenId = tokenId;                           
+        fnft.fractionalToken = address(_fip);
+        nft[tokenId]  = fnft;                                    //bind the fractional token address to this NFT token just minted
     }
 
     // The following functions are overrides required by Solidity.
@@ -80,7 +87,7 @@ contract intellectualProperty is ERC721, ERC721URIStorage, ERC721Burnable, Ownab
     function transferOwnership(address newOwner)
         public
         override(Ownable)
-        onlyOwner
+        onlySingleMember
     {
         require(newOwner != address(0), "Invalid address"); // Ensure new owner address is valid
         transferOwnership(newOwner); // Transfer ownership using Ownable
@@ -89,11 +96,11 @@ contract intellectualProperty is ERC721, ERC721URIStorage, ERC721Burnable, Ownab
     // destroy the NFT in circulation
     function burn(uint256 tokenId)
         public
-        onlyMember
+        onlySingleMember
         override(ERC721Burnable)
     {
-        require(numSupplied[msg.sender] > 0, "Not authorized to burn");
-        numSupplied[msg.sender]--;
+        require(numSupplied > 0, "Not authorized to burn");
+        numSupplied--;
 
         super._burn(tokenId);
     }
