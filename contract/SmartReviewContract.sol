2d830729cc8e5554a987bbea01fbfcebc4234bfa
xius666 <xiushiyu666@gmail.com>
2024-03-16 17:16:24 -0400

    add bouty realted code

--- contract/SmartReviewContract.sol
+++ contract/SmartReviewContract.sol
@@ -5,11 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "contract/VotingDAO.sol";
 
 contract SmartReviewContract  {
-
-    mapping (uint => SmartReview) public SmartReviewsMapping; // id-> SmartReview
-    mapping (uint => Review[]) public ReviewsMapping; // id -> Reviews
+    //storage type
+    mapping (uint => SmartReview) public SmartReviewsMapping; // id-> SmartReview 
+    mapping (uint => Review[]) public ReviewsMapping; // id -> array of Reviews
     uint id_counter_smartReview = 0; // counter for SmartReview id
-    // uint id_counter_Review = 0; //counter for Reviews is
     enum SmartReveiwPhases {ACTIVE, PAUSED, EXPIRED, PAID}
     enum ReveiwPhases {ACTIVE, ACCEPTED}
 
@@ -21,6 +20,7 @@ contract SmartReviewContract  {
         uint256 deadline;
         SmartReveiwPhases phase;
         uint256 bountyAmount;
+        uint256 currentBalance; // initially 0
     }
     struct Review{
         address payable issuer;
@@ -29,6 +29,7 @@ contract SmartReviewContract  {
     }
     event SmartReviewPublished(address payable[] issuers, string ipHash, string requirementsHash, uint256 deadline);
     event ReviewPublished(uint SmartReviewId, uint ReviewId, address payable issuer, string reviewFileHash);
+    
 
     function getSmartReviewsCount() external view returns(uint256) {
         return AllSmartReviews.length;
@@ -36,22 +37,39 @@ contract SmartReviewContract  {
     function getSmartReviewById(uint id) external view returns(SmartReview memory) {
         return SmartReviewsMapping[id];
     }
-    function getReviewsById(uint smartReviewId) external view returns(Review[] memory) {
+    function getReviewsBySmartReviewId(uint smartReviewId) external view returns(Review[] memory) { 
         return ReviewsMapping[smartReviewId];
+    }
+    function getReviewsCountBySmartReviewId(uint smartReviewId) external view returns(uint256) { 
+        return ReviewsMapping[smartReviewId].length;
     }
 
     function publishSmartReview(address payable[] calldata issuers, string calldata ipHash, string calldata requirementsHash, uint256 deadline, uint256 bountyAmount) external returns(bool) {  
-        SmartReview memory newReview = SmartReview(issuers, ipHash, requirementsHash, deadline, SmartReveiwPhases.ACTIVE, bountyAmount);
+        SmartReview memory newReview = SmartReview(issuers, ipHash, requirementsHash, deadline, SmartReveiwPhases.PAUSED, bountyAmount, 0);
         SmartReviewsMapping[id_counter_smartReview] = newReview;
         AllSmartReviews.push(newReview);
         id_counter_smartReview++;
         //emit event
         emit SmartReviewPublished(issuers, ipHash, requirementsHash, deadline);
+        
+        return true;
+    }
+    function AddBountyToSmartReview(uint256 smartReviewId, uint256 amount) external returns(bool){ 
+        address payable fromAccount = payable(msg.sender);
+        // TODO: transfer the bounty amount from the address of the caller to this contract address
+        // TODO: check sender's account balance
+        // TODO: 
+        SmartReviewsMapping[smartReviewId].currentBalance += amount;
+        SmartReview memory targetSmartReview = SmartReviewsMapping[smartReviewId];
+        if(targetSmartReview.currentBalance >= targetSmartReview.bountyAmount){
+            targetSmartReview.phase = SmartReveiwPhases.ACTIVE;
+        }
         return true;
     }
     function publishReview(string calldata reviewFileHash, uint smartReviewId) external returns (bool){
         // VotingDAO votingDaoInstance = VotingDAO(msg.sender);
         Review memory Reviewobj = Review(payable(msg.sender), reviewFileHash, ReveiwPhases.ACTIVE);
+        
         ReviewsMapping[smartReviewId].push(Reviewobj);
 
         //emit event
@@ -60,4 +78,5 @@ contract SmartReviewContract  {
         // votingDaoInstance.createProposal();
         return true;
     }
+    
 }
