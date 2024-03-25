61e57e03d7d2542baf6aea0ae045328f836bb9d3
xius666 <xiushiyu666@gmail.com>
2024-03-25 01:38:16 -0400

    remove import in smartreviewcontract.sol

--- contract/SmartReviewContract.sol
+++ contract/SmartReviewContract.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "contract/VotingDAO.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
